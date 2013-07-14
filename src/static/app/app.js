67c2b4869080b1f5d7851d2ae14c0fb26facdfba
Denis Shvets <denis.shvets.a@gmail.com>
2013-07-15 00:10:34 +0300

    Hotfix for Home Page Template #2

--- src/static/app/app.js
+++ src/static/app/app.js
@@ -2,7 +2,7 @@ angular.module('doWebNow', [])
     .config(['$routeProvider', function($routeProvider){
         $routeProvider
             .when('/', {
-                templateUrl: '/static/partials/home.html',
+                templateUrl: '/static/partials/home/home.html',
                 controller: HomeController
             })
             .otherwise({
