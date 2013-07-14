ec59dc2d7a20576f1450b01ca44cba2eca60c099
Mikhail Ivanov <team@dowebnow.com>
2013-07-14 19:07:26 +0300

    l18n deleted

--- src/news/models.py
+++ src/news/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class News(models.Model):
     class Meta:
-        verbose_name = _('News')
-        verbose_name_plural = _('News')
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
 
     title = models.CharField(max_length=200)
     full_text = models.TextField()
