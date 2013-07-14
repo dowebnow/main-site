bdc8682a844ede561ad8a3d8911f4c932c7c9a12
Mikhail Ivanov <team@dowebnow.com>
2013-07-15 00:18:07 +0300

    Added test.json

--- src/index/views.py
+++ src/index/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.utils import simplejson
 
 def index(request):
     template = loader.get_template('index/index.html')
@@ -7,3 +8,10 @@ def index(request):
         
     })
     return HttpResponse(template.render(context))
+
+def test(request):
+    data = {
+        'some_string': 'Hello World!',
+    }
+    responce = simplejson.dumps(data)
+    return HttpResponse(responce, mimetype='application/json')
