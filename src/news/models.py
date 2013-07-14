e42b585b858c53e802d960890a427abf4f05754b
Mikhail Ivanov <team@dowebnow.com>
2013-07-14 16:21:19 +0300

    News class init

--- src/news/models.py
+++ src/news/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class News()
+class News(models.Model)
     title = models.CharField(max_length=200)
     full_text = models.TextField()
     published_date = models.DateTimeField('date published')
