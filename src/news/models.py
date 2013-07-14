6727f446aefa69b6d3743bd6edfd143a88fa3c1d
Mikhail Ivanov <team@dowebnow.com>
2013-07-14 19:06:35 +0300

    Added correct plural name form

--- src/news/models.py
+++ src/news/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
+    class Meta:
+        verbose_name = _('News')
+        verbose_name_plural = _('News')
+
     title = models.CharField(max_length=200)
     full_text = models.TextField()
     published_date = models.DateTimeField('date published')
