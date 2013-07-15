879880ec4c2671d7e2247b53040bc19facf7be2f
Denis Shvets <denis.shvets.a@gmail.com>
2013-07-15 23:25:20 +0300

    Added Functionality to Menu

--- src/static/app/menu/MenuController.js
+++ src/static/app/menu/MenuController.js
@@ -1,3 +1,26 @@
-function MenuController($scope){
+function MenuController($scope, $){
     $scope.vars = {};
+
+    $scope.vars.menuItems = [
+        {
+            'name': 'Home',
+            'link': '/'
+        },
+        {
+            'name': 'Test',
+            'link': '/test'
+        }
+    ];
+
+    $scope.vars.toggleSelect = function(link){
+        selectedLink = link;
+    };
+
+    $scope.vars.getClass = function(link){
+        if( link === selectedLink ){
+            return "active";
+        } else {
+            return "";
+        };
+    };
 };
