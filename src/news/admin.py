e8140cefd7d18567b46789dd19c4a458ca442b89
Mikhail Ivanov <team@dowebnow.com>
2013-07-15 23:55:06 +0300

    Changes in News model

--- src/news/admin.py
+++ src/news/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from news.models import News
 
 class NewsAdmin(admin.ModelAdmin):
-    fields = ['title', 'published_date']
+    list_display = ('title', 'full_text', 'published_date')
 
 admin.site.register(News, NewsAdmin)
--- src/news/models.py
+++ src/news/models.py
@@ -9,3 +9,6 @@ class News(models.Model):
     title = models.CharField(max_length=200)
     full_text = models.TextField()
     published_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.title
