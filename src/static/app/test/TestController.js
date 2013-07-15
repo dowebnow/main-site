d4edf15aedaf9f737ff70f08c38864399d4303a5
Denis Shvets <denis.shvets.a@gmail.com>
2013-07-15 23:58:13 +0300

    Added More Posts

--- src/static/app/test/TestController.js
+++ src/static/app/test/TestController.js
@@ -2,9 +2,7 @@ function TestController($scope, $http){
     $scope.vars = {};
 
     $http.get('/test.json').success(function(data){
-        $scope.vars.title = data[0].fields.title;
-        $scope.vars.content = data[0].fields.full_text;
-        $scope.vars.date = data[0].fields.published_date;
+        $scope.vars.data = data;
     });
 
 };
