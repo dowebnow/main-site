2ed933997407df194759cc9bb7483db3cebd3856
Mikhail Ivanov <team@dowebnow.com>
2013-07-15 22:39:55 +0300

    Fetching News from DB

--- src/index/views.py
+++ src/index/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 import json
 
+from news import News
+
 def index(request):
     template = loader.get_template('index/index.html')
     context = RequestContext(request, {
@@ -10,8 +12,6 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def test(request):
-    data = {
-        'some_string': 'Hello World!',
-    }
-    responce = json.dumps(data)
+    news = News.objects.get()
+    responce = json.dumps(news)
     return HttpResponse(responce, mimetype='application/json')
