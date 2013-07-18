656663a8068328c4f570f7d0b89df3b9702a2c64
Mikhail Ivanov <landerexe@gmail.com>
2013-07-19 01:31:10 +0300

    Work on News posts

--- src/news/views.py
+++ src/news/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.utils.encoding import smart_unicode
+import json
+
+from news.models import News
+
+def posts_json(request):
+    news = News.objects.all()
+    json_template = loader.get_template('news/news.json')
+
+    context = RequestContext(request, {
+        'news': news,
+        'count': news.count(),
+    })
+
+    posts = json_template.render(context)
+
+    response = HttpResponse(posts, mimetype='application/json')
+    return response
