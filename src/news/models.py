b9b38cd78b4c2f22fb2ee5aeaec2cb6f49e0a039
Mikhail Ivanov <landerexe@gmail.com>
2013-07-28 16:25:24 +0300

    Choice for Authors

--- src/news/models.py
+++ src/news/models.py
@@ -2,15 +2,22 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
+
+    AUTHORS = (
+        ('dowebnow', 'doWebNow'),
+        ('mikhail_ivanov', 'Mikhail Ivanov'),
+        ('denis_shvets', 'Denis Shvets'),
+    )
+
     class Meta:
         verbose_name = 'News'
         verbose_name_plural = 'News'
 
     title = models.CharField(max_length=200)
     full_text = models.TextField()
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, choices=AUTHORS, default='dowebnow')
     image = models.ImageField(upload_to='posts_images/')
     published_date = models.DateTimeField(auto_now_add=True, verbose_name='date published')
 
     def __unicode__(self):
-        return str(self.pk) + self.title
+        return self.title
